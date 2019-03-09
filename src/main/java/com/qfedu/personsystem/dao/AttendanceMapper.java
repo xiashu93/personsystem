dcff62e4a57c0f701c5effa6ec7c05d58513af86
admin <1005804877@qq.com>
2019-03-09 19:47:07 +0800

    用户登录

--- src/main/java/com/qfedu/personsystem/dao/AttendanceMapper.java
+++ src/main/java/com/qfedu/personsystem/dao/AttendanceMapper.java
@@ -1,6 +1,7 @@
 package com.qfedu.personsystem.dao;
 
 import com.qfedu.personsystem.entity.Attendance;
+import com.qfedu.personsystem.entity.User;
 
 public interface AttendanceMapper {
     int deleteByPrimaryKey(Integer aid);
@@ -15,4 +16,6 @@ public interface AttendanceMapper {
     int updateByPrimaryKeySelective(Attendance record);
 
     int updateByPrimaryKey(Attendance record);
+
+    User selectName(String name);
 }
