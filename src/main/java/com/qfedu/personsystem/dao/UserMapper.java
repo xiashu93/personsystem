ce5af61f06707867f0647690705fe5c5c4e3ed1b
admin <1005804877@qq.com>
2019-03-09 20:08:32 +0800

    我刚才在dao层添加一个备注

--- src/main/java/com/qfedu/personsystem/dao/UserMapper.java
+++ src/main/java/com/qfedu/personsystem/dao/UserMapper.java
@@ -5,6 +5,7 @@ import com.qfedu.personsystem.entity.User;
 public interface UserMapper {
     int deleteByPrimaryKey(Integer uid);
 
+    //添加一直无尽空间
     int insert(User record);
 
     int insertSelective(User record);
