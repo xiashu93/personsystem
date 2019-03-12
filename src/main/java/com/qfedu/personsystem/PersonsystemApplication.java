e7a4140c72dd0f542aece3d22725580ee5f98835
马双文 <907691625@qq.com>
2019-03-12 17:49:49 +0800

    war包

--- src/main/java/com/qfedu/personsystem/PersonsystemApplication.java
+++ src/main/java/com/qfedu/personsystem/PersonsystemApplication.java
@@ -6,29 +6,26 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.omg.PortableInterceptor.Interceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Properties;
 
 @SpringBootApplication
 @MapperScan("com.qfedu.personsystem.dao")
-public class PersonsystemApplication {
+public class PersonsystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PersonsystemApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
 
-     @Bean
-     public PageHelper pageHelper(){
-                PageHelper pageHelper = new PageHelper();
-                 Properties properties = new Properties();
-               properties.setProperty("offsetAsPageNum","true");
-               properties.setProperty("rowBoundsWithCount","true");
-                 properties.setProperty("reasonable","true");
-                 properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
-                 pageHelper.setProperties(properties);
-                 return pageHelper;
+
+        return builder.sources(PersonsystemApplication.class);
     }
+
 
 }
